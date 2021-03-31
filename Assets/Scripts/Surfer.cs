ac953c283d3237f64317231b4beb2920a6184c6f
Liubomyr <Lubenemkrkm@gmail.com>
2021-03-31 13:30:56 +0300

    before changes

--- Assets/Scripts/Surfer.cs
+++ Assets/Scripts/Surfer.cs
@@ -1,5 +1,34 @@
+using System;
 using UnityEngine;
 
-public class Surfer : MonoBehaviour {
-    
+public class Surfer : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementVector;
+    private Camera mainCamera;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float xAxisLimit = 0.2f;
+    [SerializeField] private float DebugXaxis;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        float xAxis = 0f;
+
+        if (Input.GetMouseButton(0))
+            xAxis = Input.GetAxis("Mouse X");
+        else
+            xAxis = 0f;
+
+        Mathf.Clamp(xAxis, -xAxisLimit, xAxisLimit);
+
+        DebugXaxis = xAxis;
+
+        _movementVector = Vector3.Lerp(_movementVector, new Vector3(xAxis, 0f, 0f), _speed * Time.deltaTime);
+
+        transform.Translate(_movementVector);
+    }
 }
